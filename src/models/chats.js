a8de7386eb60c209221e788f99b45307ba30a4f1
VicentCode <vicentcode@gmail.com>
2023-12-09 23:34:57 -0600

    Add feature date message historial

--- src/models/chats.js
+++ src/models/chats.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const luxon = require('luxon');
+const luxon = require("luxon");
 
 const messageSchema = new Schema({
-    user: String,
-    message: String,
-    timestamp: { type: Date, default: Date.now }
+  user: String,
+  message: String,
+  timestamp: { type: Date, default: Date.now },
 });
 
-messageSchema.virtual('formattedTimestamp').get(function () {
-    const date = luxon.DateTime.fromJSDate(this.timestamp);
-    const now = luxon.DateTime.local();
+messageSchema.virtual("formattedTimestamp").get(function () {
+  const date = luxon.DateTime.fromJSDate(this.timestamp);
+  const now = luxon.DateTime.local();
 
-    // Si el mensaje es enviado dentro del día en curso, solo muestra la hora
-    if (date.hasSame(now, 'day')) {
-        return date.toFormat('HH:mm');
-    }
+  // Si el mensaje es enviado dentro del día en curso, solo muestra la hora
+  if (date.hasSame(now, "day")) {
+    return date.toFormat("HH:mm");
+  }
 
-    // Si el mensaje tiene menos de una semana, muestra el nombre del día y la hora
-    if (date.diff(now, 'days').days < 7) {
-        return date.toFormat('EEEE HH:mm');
-    }
+  // Si el mensaje tiene menos de una semana, muestra el nombre del día y la hora
+  if (date.diff(now, "days").days < 7) {
+    return date.toFormat("EEEE HH:mm");
+  }
 
-    // Si el mensaje tiene más de una semana, muestra el número del día, el nombre del mes y el año
-    return date.toFormat('d LLLL yyyy');
+  // Si el mensaje tiene más de una semana, muestra el número del día, el nombre del mes y el año
+  return date.toFormat("d LLLL yyyy");
 });
 
-module.exports = mongoose.model('message', messageSchema);
+module.exports = mongoose.model("message", messageSchema);
